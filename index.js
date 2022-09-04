56be7216b957a1a44300595ac75a2bccea98ac02
Szymon Wronski <szymoon.wronski@gmail.com>
2022-09-04 16:41:59 +0200

    display id of created post

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: process.env.DB_BASE
+    database: process.env.DB_BASE,
+    multipleStatements: true
 })
 
 const PORT = process.env.PORT || 5555
@@ -22,25 +23,25 @@ app.get('/', (req, res) => {
     res.status(400).sendFile(__dirname + '/help.txt')
 })
 
-app.get('/:id', (req, res) => {   
+app.get('/:id', (req, res) => {
     pool.query('SELECT code FROM codes WHERE id = ?', [req.params.id], (qErr, qRes) => {
-        if(qErr) throw qErr
-        if(isEmpty(qRes)) { res.status(404).send('404 Not Found'); return }
-        res.set({'Content-Type': 'text/plain'}).status(200).send(qRes[0].code)
+        if (qErr) throw qErr
+        if (isEmpty(qRes)) { res.status(404).send('404 Not Found'); return }
+        res.set({ 'Content-Type': 'text/plain' }).status(200).send(qRes[0].code)
     })
 })
 
 app.post('/', (req, res) => {
-    if(isEmpty(req.body)) { res.status(400).send('400 Bad Request'); return }
-    pool.query('INSERT INTO codes (code) VALUES (?)', [req.body], (qErr, qRes) => {
-        if(qErr) throw qErr
-        res.status(201).send('201 Created')
+    if (isEmpty(req.body)) { res.status(400).send('400 Bad Request'); return }
+    pool.query('INSERT INTO codes (code) VALUES (?); SELECT id FROM codes WHERE id = SCOPE_IDENTITY();', [req.body], (qErr, qRes) => {
+        if (qErr) throw qErr
+        res.status(201).send(`Shared code ID: ${qRes[1][0].id}`)
     })
 })
 
 function isEmpty(obj) {
-    for(var prop in obj) {
-        if(obj.hasOwnProperty(prop)) return false;
+    for (var prop in obj) {
+        if (obj.hasOwnProperty(prop)) return false;
     }
     return true;
 }
